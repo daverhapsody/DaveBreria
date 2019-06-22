2f30c567e83ee99d19a1182c57f9de6761222d16
daverhapsody <dave.care27@gmail.com>
2019-06-22 03:08:14 +0200

    Azzerata test

--- Test.java
+++ Test.java
@@ -1,10 +1,5 @@
-import java.util.ArrayList;
 class Test extends Dave {
   public static void main(String[] args) {
-    stampa("Tempo di allocazione di un intero: ");
-    long inizio = nanosecondiAdesso();
-    int a = 5;
-    stampa(nanosecondiAdesso()-inizio);
-    stampaACapo("ns");
+    stampaACapo("Prova");
   }  
 }
