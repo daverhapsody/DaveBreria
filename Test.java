81bbf06553ab9ccc9b20349e3534a80d3bfc4d36
daverhapsody <dave.care27@gmail.com>
2019-06-24 00:24:44 +0200

    Fatte alcune prove di tempi

--- Test.java
+++ Test.java
@@ -1,5 +1,32 @@
 class Test extends Dave {
   public static void main(String[] args) {
-    stampaACapo("Prova");
+  	long tempo, fine, inizio;
+  	int a;
+  	
+  	inizio = nanosecondiAdesso();
+  	System.out.println("Ciao");
+  	fine = nanosecondiAdesso();
+  	stampaACapo(fine-inizio +" ns");
+
+  	inizio = nanosecondiAdesso();
+  	stampaACapo("Ciao");
+  	fine = nanosecondiAdesso();
+  	stampaACapo(fine-inizio +" ns");inizio = nanosecondiAdesso();
+  	
+  	separatore();
+
+  	inizio = nanosecondiAdesso();
+  	a = somma(10,10);
+  	fine = nanosecondiAdesso();
+  	stampaACapo(fine-inizio +" ns");
+
+  	inizio = nanosecondiAdesso();
+  	a = sommaRicorsiva(10,10);
+  	fine = nanosecondiAdesso();
+  	stampaACapo(fine-inizio +" ns");
+  	inizio = nanosecondiAdesso();
+  	a = 10+10;
+  	fine = nanosecondiAdesso();
+  	stampaACapo(fine-inizio +" ns");
   }  
 }
