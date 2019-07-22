dc3a81e968fbab1dd5af771021fdcbc0f994527e
daverhapsody <dave.care27@gmail.com>
2019-07-23 01:13:51 +0200

    Primo commit sul ramo testing

--- Test.java
+++ Test.java
@@ -2,5 +2,9 @@ class Test extends Dave {
   public static void main(String[] args) {
   	System.out.println("Last Save: MacBook 23 Luglio 2019 01:06");
   	stampaACapo("Hai la faccia da culo");
+  	int a, b;
+  	a = 5;
+  	b = 6 * a;
+  	stampaACapo(b);
   }  
 }
