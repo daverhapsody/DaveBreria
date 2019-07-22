b7db10a317b6a6d5c2b511ede14c7cf032a7fce9
daverhapsody <dave.care27@gmail.com>
2019-07-23 01:07:22 +0200

    Aggiornato data di aggiornamento

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test extends Dave {
   public static void main(String[] args) {
-  	System.out.println("Last Save: MacBook");
+  	System.out.println("Last Save: MacBook 23 Luglio 2019 01:06");
   	stampaACapo("Hai la faccia da culo");
   }  
 }
