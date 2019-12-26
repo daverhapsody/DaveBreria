65c890316c144e1dc66e44860a19242e631545b2
dave <dave.care27@gmail.com>
2019-12-26 15:46:13 +0100

    pulizia

--- SoloDave.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-while true; do
-  clear
-  javac Dave.java && echo "Compilazione completata" && java Dave
-  #jar -cvf Dave.jar Dave.class && java -cp ./DaveBreria/Dave.jar Dave
-  echo "Premi invio per continuare... "
-  read
-done
