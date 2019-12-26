ecad1cda19399aa39e05efd480fb6e29b684b33e
dave <dave.care27@gmail.com>
2019-12-26 15:42:06 +0100

    pulizia

--- SoloDave.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd /Users/daveskidrow/Documents/OneDrive\ -\ Universita\ degli\ Studi\ di\ Milano-Bicocca/Univeristà/Programmazione/DaveBreria/
-while true; do
-  clear
-  javac Dave.java && echo "Compilazione completata" && java Dave
-  #jar -cvf Dave.jar Dave.class && java -cp ./DaveBreria/Dave.jar Dave
-  echo "Premi invio per continuare... "
-  read
-done
